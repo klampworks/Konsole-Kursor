a16e87bdf4393bbcd07d24dcbcd512cff438e098
yukipyon <wolfvbear@gmail.com>
2014-01-04 15:27:40 +0000

    Extended the function call with an argument and return value.

--- call_lua.cpp
+++ call_lua.cpp
@@ -4,12 +4,19 @@ extern "C" {
 	#include <lauxlib.h>
 }
 
+#include <iostream>
+
 int main() {
 
 	lua_State *L = lua_open();
 	luaL_openlibs(L);   
 	luaL_loadfile(L, "hello_world.lua");
 	lua_pcall(L, 0, 0, 0);
-	lua_getglobal(L, "hello");
-	lua_pcall(L, 0, 0, 0);
+
+	lua_getglobal(L, "square");
+	lua_pushnumber(L, 6);
+	lua_pcall(L, 1, 1, 0);
+
+	int result = lua_tonumber(L, -1);
+	std::cout << "Returned " << result << std::endl;
 }
