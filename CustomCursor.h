27f8e19874bccfbc1597333962ca1f28d8e5e240
yukipyon <wolfvbear@gmail.com>
2014-01-06 19:28:45 +0000

    Added testing code.

--- CustomCursor.h
+++ CustomCursor.h
@@ -3,8 +3,20 @@
 
 namespace CustomCursor {
 
-	void draw_cursor(QPainter &painter, const QRect &cursorRect) {
+	inline void draw_cursor(QPainter &painter, const QRect &cursorRect) {
 
+		int centre = cursorRect.left() + ((cursorRect.right() - cursorRect.left()) / 2);
+		painter.drawLine(centre,
+			cursorRect.top(),
+			centre,
+			cursorRect.bottom());
+
+		centre = cursorRect.top() + ((cursorRect.bottom() - cursorRect.top()) / 3);
+
+		painter.drawLine(cursorRect.left(),
+			centre,
+			cursorRect.right(),
+			centre);
 	}
 }
 #endif
