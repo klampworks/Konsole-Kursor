c62458cb21294e74fa0be7384eaa5470c66245b9
yukipyon <wolfvbear@gmail.com>
2014-01-08 18:03:01 +0000

    Added latin cross function.

--- CustomCursor.h
+++ CustomCursor.h
@@ -3,20 +3,34 @@
 
 namespace CustomCursor {
 
+	void latin_cross(QPainter&, const QRect&);
+
 	inline void draw_cursor(QPainter &painter, const QRect &cursorRect) {
 
-		int centre = cursorRect.left() + ((cursorRect.right() - cursorRect.left()) / 2);
-		painter.drawLine(centre,
+		latin_cross(painter, cursorRect);
+	}
+
+	inline void latin_cross(QPainter &painter, const QRect &cursorRect) {
+
+		int axis;
+
+		/* Find the horizontal centre of the drawing area. */
+		axis = cursorRect.left() + ((cursorRect.right() - cursorRect.left()) / 2);
+
+		/* Draw a vertial line alone the given axis. */
+		painter.drawLine(axis,
 			cursorRect.top(),
-			centre,
+			axis,
 			cursorRect.bottom());
 
-		centre = cursorRect.top() + ((cursorRect.bottom() - cursorRect.top()) / 3);
+		/* Find the point 30% from the top. */
+		axis = cursorRect.top() + ((cursorRect.bottom() - cursorRect.top()) / 3);
 
+		/* Draw a horizontal line along the given axis. */
 		painter.drawLine(cursorRect.left(),
-			centre,
+			axis,
 			cursorRect.right(),
-			centre);
+			axis);
 	}
 }
 #endif
