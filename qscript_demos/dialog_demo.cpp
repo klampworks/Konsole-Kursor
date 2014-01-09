46288f9ef7474ea218ec1bfd5c79e09d11d8b94b
yukipyon <wolfvbear@gmail.com>
2014-01-09 20:00:05 +0000

    Tried wrapping code inside a Object and passing it to QtScript but this does not seem to work.

--- qscript_demos/dialog_demo.cpp
+++ qscript_demos/dialog_demo.cpp
@@ -4,16 +4,29 @@
 #include <QtScript/QScriptEngine>
 #include <QtScript/QScriptValue>
  #include <QFile>
+ #include <iostream>
+
+struct my_obj : public QObject {
+
+	void show() {
+		std::cout << "Hello from cpp." << std::endl;
+		}
+	
+};
 
 int main()
 {
+
+	my_obj obj;
+
 	QApplication app(0, NULL);
 
 	QDialog cpp_dialog;
 
 	QScriptEngine engine;
 
-	QScriptValue js_dialog = engine.newQObject(&cpp_dialog);
+
+	QScriptValue js_dialog = engine.newQObject(&obj);
         engine.globalObject().setProperty("js_dialog", js_dialog);
 
 	QString script_file_name("qdialog_demo.js");
